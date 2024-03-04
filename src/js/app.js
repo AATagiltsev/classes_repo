f8c96cf665cf860f7528c9002940797acaa9da75
Alexander Tagiltsev <alextagiltsev@yandex.ru>
2024-03-04 11:13:47 +0300

    Character commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import './character.js';
+import './bowman.js';
+import './daemon.js';
+import './magician.js';
+import './swordsman.js';
+import './undead.js';
+import './zombie.js';
---
+++ src/js/character.js
@@ -0,0 +1,37 @@
+export default class Character {
+    constructor(name, type) {
+        if (typeof (name) !== 'string' || name.length < 2 || name.length > 10) {
+            throw new Error('строка, min - 2 символа, max - 10');
+        }
+
+        if (!['Bowman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie'].includes(type)) {
+            throw new Error("Должен быть один из типов: ['Bowman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie']");
+        }
+
+        this.name = name;
+        this.type = type;
+        this.health = 100;
+        this.level = 1;
+        this.attack = null;
+        this.defence = null;
+    }
+
+    levelUp() {
+        if (this.health === 0) {
+            throw new Error('Показатель жизни равен 0');
+        }
+
+        this.level += 1;
+        this.attack = this.attack + (this.attack * 20) / 100;
+        this.defence = this.defence + (this.defence * 20) / 100;
+        this.health = 100;
+    }
+
+    damage(points) {
+        if (this.health >= 0) {
+            this.health -= points * (1 - this.defence / 100);
+        } else  {
+            throw new Error('Показатель жизни равен или менее 0');
+        }
+    }
+}
